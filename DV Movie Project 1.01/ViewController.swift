0912640f86b10be5cc80c7ac5460c97b4a2ea7c7
XEducat <113421299+XEducat@users.noreply.github.com>
2023-05-24 09:30:42 +0300

    Initial Commit

---
+++ DV Movie Project 1.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DV Movie Project 1.01
+//
+//  Created by Vadim Sychevskyi  on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
